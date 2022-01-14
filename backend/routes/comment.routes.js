5e4845938b0439eeba6c87e825e2a59bd636cf1a
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-01-14 17:45:00 +0100

    Routes comments

---
+++ backend/routes/comment.routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const commentController = require('../controllers/comment.controller');
+const auth = require('../middleware/auth.middleware');
+
+
+
+
+
+
+
+
+module.exports = router;
