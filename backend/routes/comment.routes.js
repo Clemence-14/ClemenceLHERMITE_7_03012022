aaa27932c1e173936973a602bc3410d801b16887
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-01-17 13:06:14 +0100

    Routes comments

--- backend/routes/comment.routes.js
+++ backend/routes/comment.routes.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 const commentController = require('../controllers/comment.controller');
 const auth = require('../middleware/auth.middleware');
 
-
+// CRUD comments
+router.get('/allcomments', commentController.getAllComments);
+router.get('/:id', commentController.getOneComment);
+router.post('/:id', commentController.createComment);
+router.delete('/:id', commentController.deleteOneComment);
+router.put('/:id', commentController.updateComment);
 
 
 
