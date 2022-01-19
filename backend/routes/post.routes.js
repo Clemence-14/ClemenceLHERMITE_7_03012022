db844854e19d6b4c93550fdec90834094e27a99d
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-01-19 11:45:14 +0100

    Routes get, post, put, delete pour les posts

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,36 +1,22 @@
 const router = require('express').Router();
 const postController = require('../controllers/post.controller');
 const auth = require('../middleware/auth.middleware');
+const multer = require('../middleware/multer-config');
 
 // CRUD posts
 router.get('/', postController.getAllPosts);
 router.get('/:id', postController.getOnePost)
-router.post('/', postController.createPost);
-router.put('/:id', postController.updatePost);
+router.post('/', multer, postController.createPost);
+router.put('/:id', multer, postController.updatePost);
 router.delete('/:id', postController.deletePost);
+
+
+
+
 
 // Like & unlike
 //router.post('/like-post/:id', postController.likePost);
 //router.patch('/unlike-post/:id', postController.unLikePost);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
