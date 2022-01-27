1aa4d595c9382c9b793e763f3252027bfff1f348
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-01-27 14:28:31 +0100

    Routes

--- frontend/client/src/components/Routes/index.js
+++ frontend/client/src/components/Routes/index.js
@@ -4,16 +4,18 @@ import Home from "../../pages/Home";
 import Profil from "../../pages/Profil";
 import Trending from "../../pages/Trending";
 
+
+
 const index = () => {
     return (
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/profil" exact component={Profil} />
-                <Route path="/trending" exact component={Trending} />
-                <Redirect to="/" />
-            </Switch>
-        </Router>
+       <Router>
+           <Switch>
+               <Route path="/" exact component={Home} />
+               <Route path="/profil" exact component={Profil} />
+               <Route path="/trending" exact component={Trending} />
+               <Redirect to="/" />
+           </Switch>
+       </Router>
     );
 };
 
