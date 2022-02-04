4d6e7605401efbfc6951c0ac35e230e08668d45f
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-02-04 13:39:41 +0100

    Barre de navigation

--- frontend/client/src/components/Navbar.js
+++ frontend/client/src/components/Navbar.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import Logout from "./Log/Logout";
+import { UidContext } from "./Routes/AppContext";
 
 const Navbar = () => {
+    const uid = useContext(UidContext);
+    const userData = useSelector((state) => state.userReducer)
+
+    
+    
+    
+    
     return (
         <nav>
             <div className="nav-container">
@@ -13,6 +23,26 @@ const Navbar = () => {
                         </div>
                     </NavLink>
                 </div>
+                {uid ? (
+                    <ul>
+                        <li></li>
+                        <li className="welcome">
+                            <NavLink exact to="/profil">
+                                <h5>Bienvenue {userData.pseudo}</h5>
+                            </NavLink>
+                        </li>
+                        <Logout />
+                    </ul>
+                ) : (
+                    <ul>
+                        <li></li>
+                        <li>
+                            <NavLink exact to="/profil">
+                            <i class="fas fa-sign-in-alt"></i>
+                            </NavLink>
+                        </li>
+                    </ul>
+                )}
             </div>
         </nav>
     )
