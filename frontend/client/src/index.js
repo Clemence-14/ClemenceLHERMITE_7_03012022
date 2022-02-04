0f2995a4d14a2138a939af845b6fe0dbfe0903f5
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-02-04 13:37:46 +0100

    import react, react dom..

--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.scss'
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+// Dev tools
+import {composeWithDevTools } from 'redux-devtools-extension';
+import { BrowserRouter } from 'react-router-dom';
+
+
+const store = createStore(
+  rootReducer, composeWithDevTools(applyMiddleware(thunk))
+)
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
