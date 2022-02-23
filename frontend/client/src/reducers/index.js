88f47524954d5f6b24affe63bd2e45243cf8df7f
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-02-23 14:26:50 +0100

    combine reducers

--- frontend/client/src/reducers/index.js
+++ frontend/client/src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from "redux";
 import userReducer from "./user.reducer";
 import postReducer from "./post.reducer";
 import errorReducer from "./error.reducer";
+import allPostsReducer from "./allPosts.reducer";
+import trendingReducer from "./trending.reducer"; 
 
 export default combineReducers({
     userReducer,
     postReducer,
-    errorReducer
+    errorReducer, 
+    allPostsReducer,
+    trendingReducer
 })
