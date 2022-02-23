d1594cc200c638931aa0547a7fc20c165b67329b
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-02-23 14:24:06 +0100

    Page trending

--- frontend/client/src/pages/Trending.js
+++ frontend/client/src/pages/Trending.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import LeftNav from "../components/LeftNav";
+import Card from "../components/Post/Card";
+import FriendsHint from "../components/Profil/FriendsHint";
+import { UidContext } from "../components/Routes/AppContext";
+import Trends from "../components/Trends";
+import { isEmpty } from "../components/Utils";
 
 const Trending = () => {
+    const uid = useContext(UidContext);
+    const trendList = useSelector((state) => state.trendingReducer);
+
     return (
-        <div>
-            Trending
+        <div className="trending-page">
+            <LeftNav />
+            <div className="main">
+                <ul>
+                    {!isEmpty(trendList[0]) && trendList.map((post) => <Card post={post} key={post._id} />)}
+                </ul>
+            </div>
+            <div className="right-side">
+                <div className="right-side-container">
+                    <Trends />
+                    {uid && <FriendsHint />}
+                </div>
+            </div>
         </div>
     );
 };
