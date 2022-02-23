599e3f1094766d91aa8ffda2bcacaf7b9c71fce1
Clémence LHERMITE <clemencelhermite@macbook-air-de-christopher.home>
2022-02-23 14:27:33 +0100

    GET_TRENDS

---
+++ frontend/client/src/reducers/trending.reducer.js
@@ -0,0 +1,12 @@
+import { GET_TRENDS } from "../actions/post.actions";
+
+const initialSate = {};
+
+export default function trendingReducer(state = initialSate, action) {
+    switch (action.type) {
+        case GET_TRENDS:
+            return action.payload;
+        default:
+            return state;
+    }
+}
